f0357243eaa00d996dfff37c06dd4ef4d9515e5f
bitgardengames <bitgardengames@gmail.com>
2026-09-23 09:48:52 -0600

    Fix missing vehicle seat indicator on TBC

--- HydraUI/Elements/General.lua
+++ HydraUI/Elements/General.lua
@@ -127,7 +127,9 @@ function Durability:Load() -- Maybe a setting to hide the whole frame?
 	HydraUI:CreateMover(self)
 end
 
-if (not HydraUI.IsClassic) then
+-- Some Classic clients, including Burning Crusade Anniversary, do not create
+-- the Blizzard vehicle seat frame.
+if (not HydraUI.IsClassic and VehicleSeatIndicator) then
 	local SeatIndicator = HydraUI:NewModule("Vehicle Seats")
 
 	local SetSeatIndicatorPosition = function(self, anchor, parent)
